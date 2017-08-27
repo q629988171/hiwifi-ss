e670d0d843bce4def0f503a073222831e9ebaa7b
q629988171 <113960096@qq.com>
2017-08-27 22:12:23 +0800

    【增加】只有SS 运行才会检查 YOUTUBE 连接状态

--- lib/ss-test.lua
+++ lib/ss-test.lua
@@ -2,6 +2,7 @@
 -- test if ss is working
 --
 
+local log = require "luci.log"
 local host, port = "www.youtube.com", 443
 local socket = require("socket")
 local tcp = assert(socket.tcp())
@@ -9,10 +10,19 @@ tcp:settimeout(2);
 tcp:connect(host, port);
 tcp:send("GET / HTTP/1.1\n");
 local s, status, partial = tcp:receive()
-if status == "closed" then
-    io.write('yes')
-else
-    io.write('no')
+-- ss status
+local output = io.popen('/lib/gw-shadowsocks.sh status'):read('*all')
+if output == 'running' then
+    if status == "closed" then
+        io.write('yes')
+        log.print('Connection Succeeded')
+    else
+        io.write('no')            
+        log.print('Connection Failed')
+        -- reload ss
+        log.print('Reload Shadowsocks')
+        os.execute('/etc/init.d/gw-shadowsocks restart')        
+    end
 end
 tcp:close()
 
